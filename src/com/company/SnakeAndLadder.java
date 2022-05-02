6d7b80f33feb6a22977015bdd4f2a0eae8a2101f
Om Chikane <chikaneom007@gmail.com>
2022-05-03 02:00:48 +0530

    Checked: player for a options

--- src/com/company/SnakeAndLadder.java
+++ src/com/company/SnakeAndLadder.java
@@ -1,13 +1,35 @@
 package com.company;
 
+import java.lang.management.MemoryType;
+
 public class SnakeAndLadder {
+
+
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator ");
 
-//        int startPosition = 0;
-//        System.out.println(startPosition);
+        int startPosition = 0;
+        final int noPlay = 0;
+        final int ladder = 1;
+        final int snake = 2;
 
-        double rollDice = (int)(Math.floor(Math.random()*10) % 6)+1;
-        System.out.println("number "+rollDice);
+        int playerPosition = 0;
+
+        int diceRoll =(int) (Math.floor(Math.random() *10 )%6) + 1;
+        int playerOption   = (int) (Math.floor(Math.random() * 10 )% 3);
+
+        switch (playerOption){
+            case ladder :
+                playerPosition = playerPosition +diceRoll;
+                System.out.println(playerPosition);
+                break;
+            case snake :
+                playerPosition= playerPosition - diceRoll;
+                System.out.println(playerPosition);
+
+                break;
+            default:
+                break;
+        }
     }
 }
