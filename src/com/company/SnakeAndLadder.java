b0d0f09ec851a87528fac58379244ef97413caf9
Om Chikane <chikaneom007@gmail.com>
2022-05-03 01:46:48 +0530

    Added :Rp;; a dice to get number between 1-6

--- src/com/company/SnakeAndLadder.java
+++ src/com/company/SnakeAndLadder.java
@@ -4,7 +4,10 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator ");
 
-        int startPosition = 0;
-        System.out.println(startPosition);
+//        int startPosition = 0;
+//        System.out.println(startPosition);
+
+        double rollDice = (int)(Math.floor(Math.random()*10) % 6)+1;
+        System.out.println("number "+rollDice);
     }
 }
