457e4b442022a8b6c71c53f96608a5e01e85276f
Om Chikane <chikaneom007@gmail.com>
2022-05-03 01:11:46 +0530

    Added: start at 0 position

--- src/com/company/SnakeAndLadder.java
+++ src/com/company/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Simulator ");
 
+        int startPosition = 0;
+        System.out.println(startPosition);
     }
 }
