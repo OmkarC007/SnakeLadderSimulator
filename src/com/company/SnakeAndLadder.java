a362cdd5ea44c16e5bb0c2195161e404c20faae0
Om Chikane <chikaneom007@gmail.com>
2022-05-03 00:45:16 +0530

    Welcome to Snake And Ladder Simulator

---
+++ src/com/company/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake And Ladder Simulator ");
+
+    }
+}
